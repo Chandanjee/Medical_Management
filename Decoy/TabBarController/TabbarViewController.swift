b66c702df25c34a1a428f343d8290cd18d49d7b0
Chandanjee <jeepleaser@gmail.com>
2021-12-19 19:31:15 +0530

    working on tabbar

---
+++ Decoy/TabBarController/TabbarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TabbarViewController.swift
+//  Decoy
+//
+//  Created by MAC on 19/12/21.
+//
+
+import UIKit
+
+class TabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}

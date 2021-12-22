94d510f94346fd9d7c494de43d39eb24446d7dad
Chandanjee <jeepleaser@gmail.com>
2021-12-22 23:46:30 +0530

    immage set according to role

---
+++ Decoy/ViewControllers/officialController/AnalyticReport/AllReportController/MMSSYLabourRegister.swift
@@ -0,0 +1,29 @@
+//
+//  MMSSYLabourRegister.swift
+//  Decoy
+//
+//  Created by MAC on 22/12/21.
+//
+
+import UIKit
+
+class MMSSYLabourRegister: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Decoy/ViewControllers/officialController/AnalyticReport/AllReportController/MonthCampReport.swift
@@ -0,0 +1,29 @@
+//
+//  MonthCampReport.swift
+//  Decoy
+//
+//  Created by MAC on 22/12/21.
+//
+
+import UIKit
+
+class MonthCampReport: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
